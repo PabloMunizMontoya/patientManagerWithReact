6b20fd07786e721872d70dc0c8fcaa6d0bb29de1
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-03-10 16:54:09 -0500

    create state and update state of form w useState and onChange

--- patient-manager-with-react/src/components/Form.js
+++ patient-manager-with-react/src/components/Form.js
@@ -1,8 +1,26 @@
 
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 
 
 const Form = () => {
+
+    // Create appointment state
+    const [appointment, SetAppointment] = useState({
+        pet: '',
+        owner: '',
+        date: ',',
+        time: '',
+        symptoms: ''
+    })
+
+    //function that is executed when th user writhe an input
+    const updateState = e => {
+        SetAppointment({
+            ...appointment,
+            [e.target.name] : e.target.value
+        })
+    }
+
     return ( 
         <Fragment>
             <h2>Create appointment</h2>
@@ -14,6 +32,7 @@ const Form = () => {
                     name='pet'
                     className='u-full-width'
                     placeholder='Pet name'
+                    onChange={updateState}
                 />
                 <label>Owner's Name</label>
                 <input
@@ -21,25 +40,27 @@ const Form = () => {
                     name='owner'
                     className='u-full-width'
                     placeholder='Pet owner name'
+                    onChange={updateState}
                 />
                 <label>Date</label>
                 <input
                     type='date'
                     name='date'
                     className='u-full-width'
-                    
+                    onChange={updateState}
                 />
                 <label>Time</label>
                 <input
                     type='time'
                     name='time'
                     className='u-full-width'
-                    
+                    onChange={updateState}
                 />
                 <label>Symptoms</label>
                 <textarea
                     className='u-full-width'
                     name='symptoms'
+                    onChange={updateState}
                 ></textarea>
 
                 <button
